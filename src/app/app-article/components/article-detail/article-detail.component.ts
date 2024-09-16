97eae322879b3fc54d92d805eb537795659449ed
Ray <alvi.rayhan@gmail.com>
2024-09-16 15:28:02 +0400

    Add next & previous button in article details page

--- src/app/app-article/components/article-detail/article-detail.component.ts
+++ src/app/app-article/components/article-detail/article-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ArticleService } from '../../services/article.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-article-detail',
@@ -13,17 +13,31 @@ export class ArticleDetailComponent {
 
   constructor( 
     private route: ActivatedRoute,
-    private _articleservice: ArticleService
+    private _articleservice: ArticleService,
+    private router: Router
   
   ){}
 
   ngOnInit() {
-    const articleId = this.route.snapshot.paramMap.get('id');
-    if (articleId) {
-      this._articleservice.getArticleById(articleId).subscribe((data) => {
-        this.article = data;
-      });
-    }
+    // Subscribe to route parameter changes
+    this.route.paramMap.subscribe((params) => {
+      const articleId = params.get('id');
+      if (articleId) {
+        // Fetch the article when the ID changes
+        this._articleservice.getArticleById(articleId).subscribe((data) => {
+          this.article = data;
+        });
+      }
+    });
+  }
+  goPrevious(){
+    this.router.navigate(['/article', this.article.id - 1]);
+     
+
+  }
+
+  goNext(){
+    this.router.navigate(['/article', this.article.id + 1]);
   }
 
 }
